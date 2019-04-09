abb3bd99a6dffb084b844596c517bc4635895654
EJ Ortega <ortegaenriquejose@gmail.com>
2019-04-09 20:50:42 +0800

    Update dbh.php

--- includes/libs/dbh.php
+++ includes/libs/dbh.php
@@ -1,39 +1,17 @@
 <?php
 
-  // class dbConnection {
-  //   private $db_host = "localhost";
-  //   private $db_user = "root";
-  //   private $db_pass = "123456";
-  //   private $db_name = "yes";
-
-  //   function connect() {
-  //     $conn = new mysqli($this->db_host,$this->db_user, $this->db_pass, $this->db_name);
-
-  //     if ($conn->connect_error) {
-  //       die("Connection failed: ".$conn->connect_error);
-  //     }
-  //     return $conn;
-  //   }
-  // }
-    
-  $url  = "mysql://uanqgufoe2p1rak4:lwm235m6m8ijtmf5@m7wltxurw8d2n21q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/b6z6fnfthd2pmv58";
-  $dbparts = parse_url($url);
-
-  $hostname = $dbparts['host'];
-  $username = $dbparts['user'];
-  $password = $dbparts['pass'];
-  $database = ltrim($dbparts['path'],'/');
-  
   class dbConnection {
-    function connect() {
-      // Create connection
-      $conn = new mysqli($hostname, $username, $password, $database);
+    private $db_host = "m7wltxurw8d2n21q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    private $db_user = "uanqgufoe2p1rak4";
+    private $db_pass = "lwm235m6m8ijtmf5";
+    private $db_name = "b6z6fnfthd2pmv58";
 
-      // Check connection
+    function connect() {
+      $conn = new mysqli($this->db_host,$this->db_user, $this->db_pass, $this->db_name);
+
       if ($conn->connect_error) {
-          die("Connection failed hehe: " . $conn->connect_error);
+        die("Connection failed: hehe".$conn->connect_error);
       }
-      // echo "Connection was successfully established!";
       return $conn;
     }
   }
