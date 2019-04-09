07617f717385d1d657e8a5656d4fee1484b246af
EJ Ortega <40316087+ejportega@users.noreply.github.com>
2019-04-09 19:33:08 +0800

    Update dbh.php

--- includes/libs/dbh.php
+++ includes/libs/dbh.php
@@ -1,10 +1,10 @@
 <?php
 
   class dbConnection {
-    private $db_host = "s54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    private $db_user = "mii6scoyegstu9r4";
-    private $db_pass = "wbocaa2to9l3gkca";
-    private $db_name = "k0x01vy4rhl4p237";
+    private $db_host = "m7wltxurw8d2n21q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    private $db_user = "fryabvokbvmhbo9t";
+    private $db_pass = "kf5glaf1yypttyod";
+      private $db_name = "iki2qhvn8ah9asdg";
 
     function connect() {
       $conn = new mysqli($this->db_host,$this->db_user, $this->db_pass, $this->db_name);
