7197679afcccabe3c8a22fe17744ceb1059faecc
EJ Ortega <ortegaenriquejose@gmail.com>
2019-04-09 13:41:08 +0800

    Fixed dbh/database

--- includes/libs/dbh.php
+++ includes/libs/dbh.php
@@ -3,8 +3,8 @@
   class dbConnection {
     private $db_host = "localhost";
     private $db_user = "root";
-    private $db_pass = "";
-    private $db_name = "yahwehenglishschool";
+    private $db_pass = "123456";
+    private $db_name = "yes";
 
     function connect() {
       $conn = new mysqli($this->db_host,$this->db_user, $this->db_pass, $this->db_name);
