83cec9316fed6a24e81c5efd0ca5a869d7b157b6
EJ Ortega <ortegaenriquejose@gmail.com>
2019-04-09 20:40:06 +0800

    Update dbh.php

--- includes/libs/dbh.php
+++ includes/libs/dbh.php
@@ -31,7 +31,7 @@
 
       // Check connection
       if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
+          die("Connection failed hehe: " . $conn->connect_error);
       }
       // echo "Connection was successfully established!";
       return $conn;
