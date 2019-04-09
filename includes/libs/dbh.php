dc86b7fda6f7ba3fa9adbc038269ab804d071724
EJ Ortega <40316087+ejportega@users.noreply.github.com>
2019-04-09 17:40:23 +0800

    Update dbh.php

--- includes/libs/dbh.php
+++ includes/libs/dbh.php
@@ -1,10 +1,10 @@
 <?php
 
   class dbConnection {
-    private $db_host = "localhost";
-    private $db_user = "root";
-    private $db_pass = "123456";
-    private $db_name = "yes";
+    private $db_host = "otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    private $db_user = "l1jaqgen2zks3wc0";
+    private $db_pass = "b2c0er3u92yj2mm6";
+    private $db_name = "yahwehenglishschool";
 
     function connect() {
       $conn = new mysqli($this->db_host,$this->db_user, $this->db_pass, $this->db_name);
